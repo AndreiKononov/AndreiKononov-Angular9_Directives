9dc77b1307fc5652a8157ea48e3a7a3d39bb25f8
Andrei <38653807+AndreiKononov@users.noreply.github.com>
2020-04-01 15:32:38 +0300

    ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,41 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
+    selector: 'ngIF',
     template: `
-        <label>Insert your name:   </label>
-        <input [(ngModel)]="name" placeholder="name">
-        <h1>Welcome {{name}}!</h1>
+        <p>Using *ngIF only</p>
+        <p *ngIf="condition">
+            Hello Andrei!
+        </p>
+        <p *ngIf="!condition">
+            Bye-bye Andrei(
+        </p>
+        <hr>
+        <p>The same, using *ngIF "ng-template" directive</p>
+        <p *ngIf="condition; else unset">
+            Hello Andrei!
+        </p>
+        <ng-template #unset>
+            <p>Bye-Bye Andrei(</p>
+        </ng-template>
+        <hr>
+        <p>The same, using *ngIF "ng-template" directive and then/else blocks</p>
+        <div *ngIf="condition; then thenBlock else elseBlock"></div>
+        <ng-template #thenBlock>
+            <p>Hello Andrei!</p>
+        </ng-template>
+        <ng-template #elseBlock>
+            <p>Bye-Bye Andrei</p>
+        </ng-template>
+        <button (click)="toggle()">Toggle</button>
     `
 })
 
 export class AppComponent {
-    name: string = '';
+
+    condition: boolean = true;
+
+    toggle() {
+        this.condition = !this.condition;
+    }
 }
