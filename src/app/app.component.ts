bc36ebba71fbd39b5e724fc59df034aabfad8924
Andrei <38653807+AndreiKononov@users.noreply.github.com>
2020-04-01 15:02:27 +0300

    Added additional option

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
     template: `
-        <label>Insert your name:   </label>
-        <input [(ngModel)]="name" placeholder="name">
-        <h1>Welcome {{name}}!</h1>
+        <div>
+            <!-- Two ways to describe parameters -->
+            <p bold selectedSize="38px" [defaultSize]="'14px'">
+            <!-- or the same -->  
+            <!-- <p [bold]="'28px'" [defaultSize]="'14px'"> -->
+                Hello Angular 9
+            </p>
+            <p>
+                Angular 9 provides a module architecture of app
+            </p>
+        </div>
     `
 })
 
-export class AppComponent {
-    name: string = '';
-}
+export class AppComponent { }
---
+++ src/app/bold.directive.ts
@@ -0,0 +1,44 @@
+import { Directive, HostListener, Input, HostBinding, OnInit } from '@angular/core';
+
+@Directive({
+    selector: '[bold]',
+})
+export class BoldDirective implements OnInit {
+
+
+
+
+    @Input() selectedSize = "18px";
+    // or the same
+    // @Input("bold") selectedSize = "18px";
+    @Input() defaultSize = "4px";
+
+    private fontSize: string;
+    private fontWeight = "normal";
+
+    ngOnInit() {
+        this.fontSize = this.defaultSize;
+    }
+
+    @HostBinding("style.fontSize") get getFontSize() {
+        return this.fontSize;
+    }
+
+    @HostBinding("style.fontWeight") get getFontWeight() {
+        return this.fontWeight;
+    }
+
+    @HostBinding("style.cursor") get getCursor() {
+        return "pointer";
+    }
+
+    @HostListener("mouseenter") onMouseEnter() {
+        this.fontWeight ="bold";
+        this.fontSize = this.selectedSize;
+    }
+
+    @HostListener("mouseleave") onMouseLeave() {
+        this.fontWeight = "normal";
+        this.fontSize = this.defaultSize;
+    }
+}
