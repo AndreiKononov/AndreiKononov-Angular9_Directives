5e2ee904c46c6b1538d26626394044769fedf5d9
Andrei <38653807+AndreiKononov@users.noreply.github.com>
2020-04-02 15:55:33 +0300

    Added a few comments

--- src/app/while.directive.ts
+++ src/app/while.directive.ts
@@ -6,7 +6,7 @@ export class WhileDirective {
 
     constructor(private templateRef: TemplateRef<any>, private viewContainer: ViewContainerRef) { }
 
-    @Input() set while(condition: boolean) {
+    @Input() set while(condition: boolean) { // condition comes from outside
         if (condition) {
             this.viewContainer.createEmbeddedView(this.templateRef);
         } else {
